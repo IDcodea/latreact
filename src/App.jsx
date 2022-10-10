f84bda9c885c8e2ed240d6b14d4ee0948d31358f
IDcodea <arismunandar1194@gmail.com>
2022-10-10 10:19:42 -0600

    couter usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
 import { useState } from 'react';
 import Button from './components/Button';
+import Counter from './components/Counter';
 import PlaceContentCenter from './components/PlaceContentCenter';
 
 export default function App() {
-    const [count, setCount] = useState(0);
-
-    function handleCLick() {
-        setCount((prevState) => prevState + 1);
-    }
-
     return (
         <PlaceContentCenter>
-            <h1 className='text-5xl font-bold'>{count}</h1>
-            <div className='mt-5 flex items-center gap-2'>
-                <Button onClick={handleCLick}>Count+1</Button>
-                <Button
-                    onClick={() => {
-                        handleCLick();
-                        handleCLick();
-                        handleCLick();
-                    }}>
-                    Count+3
-                </Button>
-            </div>
+            <Counter initialValue={10} />
         </PlaceContentCenter>
     );
 }
